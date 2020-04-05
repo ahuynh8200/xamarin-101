b4a7a1b287361d5bc7391a757357c2373e1ce3e3
ahuynh7 <ahuynh7@uncc.edu>
2020-04-05 19:14:58 -0400

    Notes application example
    This format and code can be used as a basis for the reporting suspicious activity.

---
+++ Notes/Notes/Models/NoteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes
+{
+    public class NoteModel
+    {
+        public string Text
+        {
+            get;
+            set;
+        }
+    }
+}
---
+++ Notes/Notes/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Notes
+{
+    public class DetailPageViewModel:INotifyPropertyChanged
+    {
+        public DetailPageViewModel()
+        {
+            ExitCommand = new Command(async() => await Application.Current.MainPage.Navigation.PopAsync());
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string noteText;
+        public string NoteText
+        {
+            get => noteText;
+
+            set
+            {
+                noteText = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(NoteText)));
+            }
+
+        }
+
+        public Command ExitCommand { get; }
+    }
+}
